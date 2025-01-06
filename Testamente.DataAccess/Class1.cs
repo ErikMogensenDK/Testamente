52efc5466e28e0a666e3b40e8f5aae5834bbcb05
ErikMogensenDK <erikmogensen1@gmail.com>
2025-01-06 21:12:42 +0100

    added dataAccesLayer and configuring of .sln

---
+++ Testamente.DataAccess/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Testamente.DataAccess;
+
+public class Class1
+{
+
+}
---
+++ Testamente.DataAccessTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Testamente.DataAccessTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
