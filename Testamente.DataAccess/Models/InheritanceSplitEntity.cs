ebbc1c3efd4dd28b9c208e44330ddb2349521daf
ErikMogensenDK <erikmogensen1@gmail.com>
2025-01-15 16:16:39 +0100

    Reverted test change, and uploaded to github using new bash script

--- Testamente.DataAccess/Models/InheritanceSplitEntity.cs
+++ Testamente.DataAccess/Models/InheritanceSplitEntity.cs
@@ -5,7 +5,6 @@ public class InheritanceSplitEntity
 	public int InheritanceSplitEntityId { get; set; }
 	public int TestatorId { get; set; }
 	Dictionary<PersonEntity, double> Inheritants { get; set; }
-	public string? Test { get; set; }
 }
 
 
